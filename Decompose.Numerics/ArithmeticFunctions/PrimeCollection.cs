702c71fb6a648ebfa8f84948f030dbb1646ce446
Rick Sladkey <rick.sladkey@gmail.com>
2012-03-06 21:39:29 +0000

    Avoid factors of three.

--- Decompose.Numerics/ArithmeticFunctions/PrimeCollection.cs
+++ Decompose.Numerics/ArithmeticFunctions/PrimeCollection.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Decompose.Numerics
 {
     public class PrimeCollection
     {
-        private const int blockSize = 1 << 18;
+        private const int blockSizeSingleThreaded = 1 << 16;
+        private const int blockSizeMultiThreaded = 1 << 18;
         private const int bucketSize = 1 << 20;
         private const int maxBuckets = 1 << 10;
 
@@ -26,17 +28,29 @@ namespace Decompose.Numerics
 
         public PrimeCollection(long size, int threads)
         {
-            this.size = size;
+            this.size = Math.Min((long)uint.MaxValue + 1, size);
             limit = (int)Math.Ceiling(Math.Sqrt(size));
             currentBucket = 0;
             currentIndex = 0;
-            block = new bool[Math.Max(blockSize >> 1, limit)];
+            block = new bool[Math.Max(blockSizeSingleThreaded >> 1, limit)];
             primes = new uint[maxBuckets][];
             primes[0] = new uint[bucketSize];
             divisors = primes[0];
+            if (size <= 13)
+            {
+                if (size > 2)
+                    AddPrime(2);
+                if (size > 3)
+                    AddPrime(3);
+                if (size > 5)
+                    AddPrime(5);
+                if (size > 7)
+                    AddPrime(7);
+                if (size > 11)
+                    AddPrime(11);
+                return;
+            }
             GetDivisors(block);
-            if (limit == 2 && limit < size)
-                AddPrime(2);
             if (threads == 0)
                 GetPrimes();
             else
@@ -60,10 +74,10 @@ namespace Decompose.Numerics
                     offset += d;
                 offsets[i] = (int)offset;
             }
-            for (var k = k0; k < size; k += blockSize)
+            for (var k = k0; k < size; k += blockSizeSingleThreaded)
             {
                 var kstart = (uint)k;
-                var length = (int)Math.Min(blockSize, size - k);
+                var length = (int)Math.Min(blockSizeSingleThreaded, size - k);
                 SieveBlock(kstart, length, block, offsets);
                 AddPrimes(kstart, length, block);
             }
@@ -72,28 +86,28 @@ namespace Decompose.Numerics
         private void GetPrimes(int threads)
         {
             var k0 = (long)limit & ~1;
-            var batchSize = threads * blockSize;
+            var batchSize = threads * blockSizeMultiThreaded;
             var tasks = new Task[threads];
             var blocks = new bool[threads][];
             for (var thread = 0; thread < threads; thread++)
-                blocks[thread] = new bool[blockSize >> 1];
+                blocks[thread] = new bool[blockSizeMultiThreaded >> 1];
             for (var l = k0; l < size; l += batchSize)
             {
                 var kmax = l + Math.Min(batchSize, size - l);
                 var thread = 0;
-                for (var k = l; k < kmax; k += blockSize)
+                for (var k = l; k < kmax; k += blockSizeMultiThreaded)
                 {
                     var kstart = (uint)k;
-                    var length = (int)Math.Min(blockSize, kmax - k);
+                    var length = (int)Math.Min(blockSizeMultiThreaded, kmax - k);
                     var block = blocks[thread];
                     tasks[thread] = Task.Factory.StartNew(() => SieveBlock(kstart, length, block));
                     ++thread;
                 }
                 thread = 0;
-                for (var k = l; k < kmax; k += blockSize)
+                for (var k = l; k < kmax; k += blockSizeMultiThreaded)
                 {
                     var kstart = (uint)k;
-                    var length = (int)Math.Min(blockSize, kmax - k);
+                    var length = (int)Math.Min(blockSizeMultiThreaded, kmax - k);
                     tasks[thread].Wait();
                     var block = blocks[thread];
                     AddPrimes(kstart, length, block);
@@ -128,8 +142,25 @@ namespace Decompose.Numerics
         private void SieveBlock(uint k0, int length, bool []block, int[] offsets)
         {
             var length2 = length >> 1;
-            Array.Clear(block, 0, length2);
-            for (var d = 1; d < numberOfDivisors; d++)
+            var dlimit = Math.Min(numberOfDivisors, 6);
+            var cycleSize = 1;
+            for (var d = 2; d < dlimit; d++)
+                cycleSize *= (int)divisors[d];
+            Array.Clear(block, 0, cycleSize);
+            for (var d = 2; d < dlimit; d++)
+            {
+                var i = (int)divisors[d];
+                var j = i - k0 % i;
+                j = (j + (((j & 1) - 1) & i)) >> 1;
+                while (j < cycleSize)
+                {
+                    block[j] = true;
+                    j += i;
+                }
+            }
+            for (var i = cycleSize; i < length2; i += cycleSize)
+                Array.Copy(block, 0, block, i, Math.Min(cycleSize, length2 - i));
+            for (var d = dlimit; d < numberOfDivisors; d++)
             {
                 var i = (int)divisors[d];
                 var j = offsets[d] >> 1;
@@ -146,13 +177,11 @@ namespace Decompose.Numerics
         {
             var length2 = length >> 1;
             Array.Clear(block, 0, length2);
-            for (var d = 1; d < numberOfDivisors; d++)
+            for (var d = 2; d < numberOfDivisors; d++)
             {
                 var i = divisors[d];
                 var j = i - k0 % i;
-                if ((j & 1) == 0)
-                    j += i;
-                j >>= 1;
+                j = (j + (((j & 1) - 1) & i)) >> 1;
                 while (j < length2)
                 {
                     block[j] = true;
@@ -161,6 +190,7 @@ namespace Decompose.Numerics
             }
         }
 
+#if false
         private void AddPrimes(uint k0, int length, bool[] block)
         {
             var length2 = length >> 1;
@@ -170,6 +200,35 @@ namespace Decompose.Numerics
                     AddPrime((uint)((i << 1) | 1) + k0);
             }
         }
+#endif
+
+#if true
+        private void AddPrimes(uint k0, int length, bool[] block)
+        {
+            var length2 = length >> 1;
+            var imin = 3 - k0 % 3;
+            imin = (imin + (((imin & 1) - 1) & 3)) >> 1;
+            var i = 0;
+            while (i < imin)
+            {
+                if (!block[i])
+                    AddPrime((uint)((i << 1) | 1) + k0);
+                ++i;
+            }
+            while (true)
+            {
+                if (++i >= length2)
+                    break;
+                if (!block[i])
+                    AddPrime((uint)((i << 1) | 1) + k0);
+                if (++i >= length2)
+                    break;
+                if (!block[i])
+                    AddPrime((uint)((i << 1) | 1) + k0);
+                ++i;
+            }
+        }
+#endif
 
         private void AddPrime(uint p)
         {
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -74,7 +74,7 @@ namespace Sandbox
             var timer = new Stopwatch();
             timer.Start();
             var n = (long)1 << 32;
-            var primes = new PrimeCollection(n + 1, 8);
+            var primes = new PrimeCollection(n + 1, 0);
             Console.WriteLine("|primes| = {0}", primes.Count, 0);
 #if false
             for (int i = 0; i < primes.Count; i++)
