7873eba9840a3c8afd6e86315d940e16158952c6
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-10-26 18:58:04 +0100

    swap bytes clean

--- Nethermind.Dirichlet.Numerics/UInt256.cs
+++ Nethermind.Dirichlet.Numerics/UInt256.cs
@@ -285,35 +285,15 @@ namespace Nethermind.Dirichlet.Numerics
             }
         }
 
-        public static ulong SwapBytes(ulong x)
-        {
-            // swap adjacent 32-bit blocks
-            x = (x >> 32) | (x << 32);
-            // swap adjacent 16-bit blocks
-            x = ((x & 0xFFFF0000FFFF0000) >> 16) | ((x & 0x0000FFFF0000FFFF) << 16);
-            // swap adjacent 8-bit blocks
-            return ((x & 0xFF00FF00FF00FF00) >> 8) | ((x & 0x00FF00FF00FF00FF) << 8);
-        }
-
-        public static ulong SwapBytes2(ulong value)
-        {
-            return (ulong) BinaryPrimitives.ReverseEndianness((long) value);
-        }
-
-        public static ulong SwapBytes3(ulong value)
-        {
-            return (ulong) IPAddress.HostToNetworkOrder((long) value);
-        }
-
         public static void CreateFromBigEndian(out UInt256 c, Span<byte> span)
         {
             Span<ulong> ulongs = MemoryMarshal.Cast<byte, ulong>(span);
             if (ulongs.Length == 4)
             {
-                c.s0 = SwapBytes2(ulongs[3]);
-                c.s1 = SwapBytes2(ulongs[2]);
-                c.s2 = SwapBytes2(ulongs[1]);
-                c.s3 = SwapBytes2(ulongs[0]);
+                c.s0 = BinaryPrimitives.ReverseEndianness(ulongs[3]);
+                c.s1 = BinaryPrimitives.ReverseEndianness(ulongs[2]);
+                c.s2 = BinaryPrimitives.ReverseEndianness(ulongs[1]);
+                c.s3 = BinaryPrimitives.ReverseEndianness(ulongs[0]);
             }
             else
             {
